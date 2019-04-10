9d64acfc499f6e4abd1c19b1c40dd69699c0dba9
Aoi Matsuba <aoi.matsuba@scout24.com>
2019-04-10 17:35:53 +0200

    Create simple app using geolocation and class component with some states

---
+++ src/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props); // paretns constructor
+
+        this.state = { latitude: null, errMessage: '' };
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                this.setState({ latitude: position.coords.latitude });
+            },
+            err => {
+                this.setState({ errMessage: err.message });
+            }
+        );
+    }
+
+    render() {
+        if (this.state.errMessage && !this.state.latitude) {
+            return <div>Error: {this.state.errMessage}</div>;
+        }
+
+        if (!this.state.errMessage && this.state.latitude) {
+            return <div>Latitude: {this.state.latitude}</div>;
+        }
+
+        return <div>Loading!</div>;
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
