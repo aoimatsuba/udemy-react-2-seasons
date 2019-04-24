e3af28b6d5a1534c4fbbca0cfeab0d995e9d667a
Aoi Matsuba <aoi.matsuba@scout24.com>
2019-04-24 16:24:04 +0200

    Refactored and added icons and styles

--- src/index.js
+++ src/index.js
@@ -1,32 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import SeasonDisplay from './SeasonDisplay'
+import Spinner from './Spinner'
 
 class App extends React.Component {
 
-    constructor(props) {
-        super(props); // paretns constructor
-
-        this.state = { latitude: null, errMessage: '' };
-    }
+    state = { lat: null, errorMessage: '' };
 
     componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ latitude: position.coords.latitude }),
+            position => this.setState({ lat: position.coords.latitude }),
             err => this.setState({ errMessage: err.message })
         );
     }
 
-    render() {
-        if (this.state.errMessage && !this.state.latitude) {
+    renderContent() {
+        if (this.state.errMessage && !this.state.lat) {
             return <div>Error: {this.state.errMessage}</div>;
         }
 
-        if (!this.state.errMessage && this.state.latitude) {
-            return <div>Latitude: {this.state.latitude}</div>;
+        if (!this.state.errMessage && this.state.lat) {
+            return <SeasonDisplay lat={this.state.lat} />
         }
 
-        return <div>Loading!</div>;
+        return <Spinner message="Please accept location request" />;
+
+    }
+
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        )
     }
 }
 
